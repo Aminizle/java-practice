b47439b97bdcb81492a9b661accbe0c018fef53e
Ameen Mohiyuddin <57292718+Aminizle@users.noreply.github.com>
2023-07-27 20:04:58 -0400

    added passing variables between x and y

---
+++ Main.java
@@ -0,0 +1,17 @@
+public class Main {
+
+    public static void main (String[] args) {
+       
+        String x= "Water";
+        String y="Cool-Aid";
+        String temp;
+
+        temp=x;
+        x=y;
+        y=temp;
+
+        System.out.println(x);
+        System.out.println(y);
+    }
+    
+}
